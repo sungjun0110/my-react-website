853b3a5be5f42e63cad03381484909064f23c6af
Sungjun Choi <sungjun0110@gmail.com>
2022-04-13 22:17:11 -0700

    cleans code

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -8,71 +8,69 @@ function About() {
   const ref = useRef();
 
   return (
-    <div id="about-shadow">
-      <div id="about-page" ref={ref}>
-        <h1>About</h1>
-        <div className="about-content">
-          <div className="about-text">
-            <p>
-              My name is Sungjun Choi, a self-taught Web Developer. I'm a guy who loves computer technology and likes to put it into practice.
-            </p>
-            <p>
-              I like to create intuitive user interface, resolve technical problems, and develop websites from scratch. While my focus is front-end technologies such as ReactJS, I also enjoy building back-end.
-            </p>
-            <p>
-              I always love to learn and experiment new skills and technologies.
-            </p>
-          </div>
+    <div id="about-page" ref={ref}>
+      <h1>About</h1>
+      <div className="about-content">
+        <div className="about-text">
+          <p>
+            My name is Sungjun Choi, a self-taught Web Developer. I'm a guy who loves computer technology and likes to put it into practice.
+          </p>
+          <p>
+            I like to create intuitive user interface, resolve technical problems, and develop websites from scratch. While my focus is front-end technologies such as ReactJS, I also enjoy building back-end.
+          </p>
+          <p>
+            I always love to learn and experiment new skills and technologies.
+          </p>
+        </div>
 
-          <div className="skill-icons">
-            <IconContext.Provider 
-              value={{
-                className: "skill-icon", 
-                size: "3.5rem", 
-                color: "var(--color5)", 
-              }}
-            >
-              <h2 id="skills-title">Skills</h2>
-              <div id="skill-div">
-                <div className="skill-icon-div">
-                  <FaJs />
-                  <p>JavaScript</p>
-                </div>
-                <div className="skill-icon-div">
-                  <FaReact />
-                  <p>React</p>
-                </div>
-                <div className="skill-icon-div">
-                  <FaHtml5 />
-                  <p>Html5</p>
-                </div>
-                <div className="skill-icon-div">
-                  <FaCss3 />
-                  <p>CSS3</p>
-                </div>
-                <div className="skill-icon-div">
-                  <FaNodeJs />
-                  <p>Node.js</p>
-                </div>
-                <div className="skill-icon-div">
-                  <FaWordpressSimple />
-                  <p>WordPress</p>
-                </div>
-                <div className="skill-icon-div">
-                  <FaPhp />
-                  <p>Php</p>
-                </div>
-                <div className="skill-icon-div">
-                  <FaPython />
-                  <p>Python</p>
-                </div>
-                <div className="skill-icon-div">
-                  <FaJava />
-                  <p>Java</p>
-                </div>
+        <div className="skill-icons">
+          <IconContext.Provider 
+            value={{
+              className: "skill-icon", 
+              size: "3.5rem", 
+              color: "var(--color5)", 
+            }}
+          >
+            <h2 id="skills-title">Skills</h2>
+            <div id="skill-div">
+              <div className="skill-icon-div">
+                <FaJs />
+                <p>JavaScript</p>
               </div>
-            </IconContext.Provider>
-          </div>
+              <div className="skill-icon-div">
+                <FaReact />
+                <p>React</p>
+              </div>
+              <div className="skill-icon-div">
+                <FaHtml5 />
+                <p>Html5</p>
+              </div>
+              <div className="skill-icon-div">
+                <FaCss3 />
+                <p>CSS3</p>
+              </div>
+              <div className="skill-icon-div">
+                <FaNodeJs />
+                <p>Node.js</p>
+              </div>
+              <div className="skill-icon-div">
+                <FaWordpressSimple />
+                <p>WordPress</p>
+              </div>
+              <div className="skill-icon-div">
+                <FaPhp />
+                <p>Php</p>
+              </div>
+              <div className="skill-icon-div">
+                <FaPython />
+                <p>Python</p>
+              </div>
+              <div className="skill-icon-div">
+                <FaJava />
+                <p>Java</p>
+              </div>
+            </div>
+          </IconContext.Provider>
         </div>
       </div>
     </div>
--- src/pages/WorkContentPage/WorkContentPage.jsx
+++ src/pages/WorkContentPage/WorkContentPage.jsx
@@ -6,62 +6,58 @@ import sosimg from '../../img/sosimg.png'
 
 function WorkContentPage() {
   return (
-    <>
-      <div id="projects-shadow">
-        <div id="projects">
-          <h1>Projects</h1>
-          <h2>Web Development</h2>
-          <div className="main-col">
-            <WorkContentBox
-              image='https://i.imgur.com/E1b4fD0.png'
-              path="/website/ui"
-              alt="ui patterns" 
-              description="UI Components"
-            />
-            <WorkContentBox 
-              image='https://camo.githubusercontent.com/d1459350931c8ae6b2f28526cd9fc1622b540f05b710d0daecf08d13d6aa741b/68747470733a2f2f692e696d6775722e636f6d2f68644476456e322e706e67' 
-              path="/website/givetake" 
-              alt="ncovtracker.com" 
-              description="API & UI Development" 
-            />
-            <WorkContentBox 
-              image='https://camo.githubusercontent.com/425d960e5e081b35841fe69f771b807ed024084ca256570004299434f007c377/68747470733a2f2f692e696d6775722e636f6d2f6b67616c55504b2e706e67' 
-              path="/website/photopin" 
-              alt="ncovtracker.com" 
-              description="API & UI Development" 
-            />
-            <WorkContentBox 
-              image='https://camo.githubusercontent.com/7b49f44b3c1770e78b0cc220ba87e85c77e8ee9e5bf5a0d3190d1033c18c551d/68747470733a2f2f692e696d6775722e636f6d2f6a54464b44626f2e706e67' 
-              path="/website/minesweeper" 
-              alt="ncovtracker.com" 
-              description="Minesweeper" 
-            />
-            <WorkContentBox 
-              image={ncovtracker} 
-              path="/website/covidtracker" 
-              alt="ncovtracker.com" 
-              description="Covid Tracker" 
-            />
-            <WorkContentBox 
-              image={sushikuma}
-              path="/website/sushikuma" 
-              alt="sushikuma.net" 
-              description="Touch-free restaurant menu" 
-            />
-          </div>
-
-          <h2>App Development</h2>
-          <div className="main-col">
-            <WorkContentBox 
-              image={sosimg}
-              path="/website/androidapp" 
-              alt="application"
-              description="Android application" 
-            />
-          </div>
-        </div>
+    <div id="projects">
+      <h1>Projects</h1>
+      <h2>Web Development</h2>
+      <div className="main-col">
+        <WorkContentBox
+          image='https://i.imgur.com/E1b4fD0.png'
+          path="/website/ui"
+          alt="ui patterns" 
+          description="UI Components"
+        />
+        <WorkContentBox 
+          image='https://camo.githubusercontent.com/d1459350931c8ae6b2f28526cd9fc1622b540f05b710d0daecf08d13d6aa741b/68747470733a2f2f692e696d6775722e636f6d2f68644476456e322e706e67' 
+          path="/website/givetake" 
+          alt="ncovtracker.com" 
+          description="API & UI Development" 
+        />
+        <WorkContentBox 
+          image='https://camo.githubusercontent.com/425d960e5e081b35841fe69f771b807ed024084ca256570004299434f007c377/68747470733a2f2f692e696d6775722e636f6d2f6b67616c55504b2e706e67' 
+          path="/website/photopin" 
+          alt="ncovtracker.com" 
+          description="API & UI Development" 
+        />
+        <WorkContentBox 
+          image='https://camo.githubusercontent.com/7b49f44b3c1770e78b0cc220ba87e85c77e8ee9e5bf5a0d3190d1033c18c551d/68747470733a2f2f692e696d6775722e636f6d2f6a54464b44626f2e706e67' 
+          path="/website/minesweeper" 
+          alt="ncovtracker.com" 
+          description="Minesweeper" 
+        />
+        <WorkContentBox 
+          image={ncovtracker} 
+          path="/website/covidtracker" 
+          alt="ncovtracker.com" 
+          description="Covid Tracker" 
+        />
+        <WorkContentBox 
+          image={sushikuma}
+          path="/website/sushikuma" 
+          alt="sushikuma.net" 
+          description="Touch-free restaurant menu" 
+        />
       </div>
-    </>
+
+      <h2>App Development</h2>
+      <div className="main-col">
+        <WorkContentBox 
+          image={sosimg}
+          path="/website/androidapp" 
+          alt="application"
+          description="Android application" 
+        />
+      </div>
+    </div>
   )
 }
 
